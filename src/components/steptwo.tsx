c747273acf81e584549270c6eb75fe19a66a937d
Ya Sin Allah <yasin.allahdev@gmail.com>
2019-12-28 22:56:42 -0500

    Successfully use MBTA API with Axios

--- src/components/steptwo.tsx
+++ src/components/steptwo.tsx
@@ -1,10 +1,10 @@
 import { Text, View, Button, useEventHandler } from "@nodegui/react-nodegui";
 import { QPushButtonSignals } from "@nodegui/nodegui";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import open from "open";
 import axios from "axios";
 
-export async function StepTwo() {
+export function StepTwo() {
   const btnHandler = useEventHandler<QPushButtonSignals>(
     {
       clicked: () => open("https://react.nodegui.org").catch(console.log)
@@ -12,34 +12,33 @@ export async function StepTwo() {
     []
   );
 
-  let currentJSX;
+  const [data, setData] = useState({ srcStation: "", terminalStation: "" });
 
-  await axios.get(`https://api-v3.mbta.com/routes/Green-B`)
-    .then(response => {
-      currentJSX = (
-        <View style={containerStyle}>
-          <Text style={textStyle} wordWrap={true}>
-            {`
-              <p>Trains originate at: ${response.data.data.attributes.direction_destinations[0]}</p>
-              <p>Trains terminate at: ${response.data.data.attributes.direction_destinations[1]}</p>
-            `}
-          </Text>
-        </View>
-      );
-    })
-    .catch(err => {
-      currentJSX =  (
-        <View style={containerStyle}>
-          <Text style={textStyle} wordWrap={true}>
-            {`
-              <p>There was an error!</p>
-            `}
-          </Text>
-        </View>
-      );
-    })
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://api-v3.mbta.com/routes/Green-B');
+      const trainData = {
+        srcStation: result.data.data.attributes.direction_destinations[0],
+        terminalStation: result.data.data.attributes.direction_destinations[1]
+      }
+      console.log(trainData);
+      console.table(trainData);
+      setData(trainData);
+    }
 
-    return currentJSX;
+    fetchData();
+  }, [])
+
+  return (
+    <View style={containerStyle}>
+      <Text style={textStyle} wordWrap={true}>
+        {`
+          <p>Trains originate at: ${data.srcStation}</p>
+          <p>Trains terminate at: ${data.terminalStation}</p>
+        `}
+      </Text>
+    </View>
+  )
 
   // return (
   //   <View style={containerStyle}>
