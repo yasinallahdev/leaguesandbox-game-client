a9a7d4bfe567db654db0ce567c39ccbfd7086d16
Ya Sin Allah <yasin.allahdev@gmail.com>
2019-12-28 22:37:43 -0500

    Attempt #1 at adding Axios

--- src/components/stepone.tsx
+++ src/components/stepone.tsx
@@ -1,27 +1,28 @@
 import { Text, View } from "@nodegui/react-nodegui";
-import React from "react";
-const dogImg = require("./dog");
+import React, { Component } from "react";
+const dogImg = require('./dog');
 
-export function StepOne() {
-  return (
-    <View style={containerStyle}>
-      <Text wordWrap={true}>
-        Edit App.tsx to make changes to this screen. Then come back to see your
-        changes. Changes should reflect live thanks to Hot Reloading. 🔥
-      </Text>
-      <Text>
-        {`
-          <p style="color: rgb(255,72,38);"> 
-            <center>
-              <img src="${dogImg}" alt="doggy" />  
-            </center>
-            <center>You can even use <i><strong>Rich Html</strong></i> text like this if you want 😎.</center>
-          </p>
-          <hr />
-        `}
-      </Text>
-    </View>
-  );
+export class StepOne extends React.Component {
+  render() { 
+    return (
+      <View style={containerStyle}>
+        <Text wordWrap={true}>
+          Hello from React NodeGUI!!!
+        </Text>
+        <Text>
+          {`
+            <p style="color: rgb(255,72,38);"> 
+              <center>
+                <img src="${dogImg}" alt="doggy" />  
+              </center>
+              <center>You can even use <i><strong>Rich Html</strong></i> text like this if you want 😎.</center>
+            </p>
+            <hr />
+          `}
+        </Text>
+      </View>
+    );
+  }
 }
 
 const containerStyle = `
--- src/components/steptwo.tsx
+++ src/components/steptwo.tsx
@@ -1,41 +1,72 @@
 import { Text, View, Button, useEventHandler } from "@nodegui/react-nodegui";
 import { QPushButtonSignals } from "@nodegui/nodegui";
-import React from "react";
+import React, { useEffect } from "react";
 import open from "open";
+import axios from "axios";
 
-export function StepTwo() {
+export async function StepTwo() {
   const btnHandler = useEventHandler<QPushButtonSignals>(
     {
       clicked: () => open("https://react.nodegui.org").catch(console.log)
     },
     []
   );
-  return (
-    <View style={containerStyle}>
-      <Text style={textStyle} wordWrap={true}>
-        {`
-          <ol>
-            <li>
-                Open chrome and navigate to chrome://inspect. You should see a target below with your app.
-            </li>
-            <br/>
-              <li>
-                  Next click on  "Open dedicated DevTools for Node"
-              </li>
-              <br/>
-            <li>
-                On the dedicated devtools. Click on Source > Node > "Your node process"
-            </li>
-          </ol>
-        `}
-      </Text>
-      <Button
-        style={btnStyle}
-        on={btnHandler}
-        text={`Open React NodeGui docs`}
-      ></Button>
-    </View>
-  );
+
+  let currentJSX;
+
+  await axios.get(`https://api-v3.mbta.com/routes/Green-B`)
+    .then(response => {
+      currentJSX = (
+        <View style={containerStyle}>
+          <Text style={textStyle} wordWrap={true}>
+            {`
+              <p>Trains originate at: ${response.data.data.attributes.direction_destinations[0]}</p>
+              <p>Trains terminate at: ${response.data.data.attributes.direction_destinations[1]}</p>
+            `}
+          </Text>
+        </View>
+      );
+    })
+    .catch(err => {
+      currentJSX =  (
+        <View style={containerStyle}>
+          <Text style={textStyle} wordWrap={true}>
+            {`
+              <p>There was an error!</p>
+            `}
+          </Text>
+        </View>
+      );
+    })
+
+    return currentJSX;
+
+  // return (
+  //   <View style={containerStyle}>
+  //     <Text style={textStyle} wordWrap={true}>
+  //       {`
+  //         <ol>
+  //           <li>
+  //               Open chrome and navigate to chrome://inspect. You should see a target below with your app.
+  //           </li>
+  //           <br/>
+  //             <li>
+  //                 Next click on  "Open dedicated DevTools for Node"
+  //             </li>
+  //             <br/>
+  //           <li>
+  //               On the dedicated devtools. Click on Source > Node > "Your node process"
+  //           </li>
+  //         </ol>
+  //       `}
+  //     </Text>
+  //     <Button
+  //       style={btnStyle}
+  //       on={btnHandler}
+  //       text={`Open React NodeGui docs`}
+  //     ></Button>
+  //   </View>
+  // );
 }
 
 const containerStyle = `
